bd4dfef89da74e173404c1866a3c80c6dc708266
unknown <chandravanshisagar28@gmail.com>
2026-06-01 21:31:12 +0530

    update api base url

--- frontend/ai-habit-tracker-ui-boilerplate-code/src/api/axios.js
+++ frontend/ai-habit-tracker-ui-boilerplate-code/src/api/axios.js
@@ -2,7 +2,7 @@
   import axios from "axios";
 
   const api = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://ai-habit-tracker-6mqf.onrender.com",
   });
 
   api.interceptors.request.use((config) => {
